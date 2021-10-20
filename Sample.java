7a2d12f14f4eb8d77f20f99e888ec0f526f739f0
anteknik <anton.takaful@gmail.com>
2021-10-20 17:26:48 +0000

    add java,python,php

---
+++ Sample.java
@@ -0,0 +1,17 @@
+class Sample{
+    public static void main(String[] args) {
+    var s="ini string";
+
+    var p=new Person();
+    p.nama="adi";
+    p.age=39;
+    System.out.println("hello :"+p.nama);
+
+    }
+
+    
+}
+class Person{
+    String nama;
+    Integer age;
+}
---
+++ test.py
@@ -0,0 +1,5 @@
+def print_details(name,age):
+    print(f"Hello {name}! How are you?")
+    print(f"You are now {age}.")
+
+print_details("anton",35)
